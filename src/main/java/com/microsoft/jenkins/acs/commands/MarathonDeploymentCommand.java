3085e9290a34d97b29a8dc8815fe7e3dd03c4140
Menghua Xiao <menxiao@microsoft.com>
2017-06-28 14:00:24 +0800

    Adding copyright information for Java source code

--- src/main/java/com/microsoft/jenkins/acs/commands/MarathonDeploymentCommand.java
+++ src/main/java/com/microsoft/jenkins/acs/commands/MarathonDeploymentCommand.java
@@ -3,6 +3,7 @@
  * Licensed under the MIT License. See License.txt in the project root for
  * license information.
  */
+
 package com.microsoft.jenkins.acs.commands;
 
 import com.cloudbees.jenkins.plugins.sshcredentials.SSHUserPrivateKey;
